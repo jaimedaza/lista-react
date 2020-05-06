7bbc0ff9d32ced831f489f8755e448bb743cbda3
Jaime Daza <jaimedaza11@gmail.com>
2020-05-05 19:46:40 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+/// Mostrar una lista en base a un arreglo
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      list: ['Javascript', 'CSS', 'HTLM', 'React']
+    }
+  }
+  render() {
+    return (
+      <div style={styles.container}>
+        <h1 style={styles.title}>Las tecnologias del Front</h1>
+        <ul style={styles.list}>
+          {this.state.list.map((name) =>
+            <li key={name}>{name}</li>            
+          )}          
+        </ul>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    marginTop: '100px'
+  },
+  list: {
+    fontSize: '20px',
+    flexGrow: 6
+  }
+}
+
+export default App;
